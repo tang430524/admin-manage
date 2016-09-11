1220543346fc5ad0fcb1a669759bd7656fb11244
不迷失 <1060420897@qq.com>
2016-09-11 22:50:42 +0800

    init

---
+++ src/main/java/org/javajidi/admin/Application.java
@@ -0,0 +1,15 @@
+package org.javajidi.admin;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by xieqiang on 2016/9/11.
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class Application {
+
+    public static void main(String[] arg){
+        SpringApplication.run(Application.class);
+    }
+}
+
