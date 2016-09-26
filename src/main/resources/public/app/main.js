f08aeb2d3ca21b33da343c997ad80a7fb20f0610
qiangxie <1060420897@qq.com>
2016-09-26 21:54:30 +0800

    update

--- src/main/resources/public/app/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Created by xieqiang on 2016/9/25.
- */
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_moudle_1 = require("./app.moudle");
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_moudle_1.AppModule);
-//# sourceMappingURL=main.js.map
