8cc46fcd851c1dc1d748acd79bd98334bfe1ef2d
不迷失 <1060420897@qq.com>
2016-10-06 14:32:51 +0800

    update sql

--- src/main/mysql.sql
+++ src/main/mysql.sql
@@ -1,24 +1,28 @@
----------------------------
--- Table structure for `menu`
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: admin_manage
+Target Host: localhost
+Target Database: admin_manage
+Date: 2016/10/6 14:31:51
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
-DROP TABLE IF EXISTS `menu`;
+-- Table structure for menu
+-- ----------------------------
 CREATE TABLE `menu` (
   `code` varchar(30) NOT NULL,
   `label` varchar(20) NOT NULL,
   `url` varchar(200) DEFAULT NULL,
-  `disable` smallint(6) DEFAULT '0',
+  `disabled` smallint(6) DEFAULT '0',
   `items` text COMMENT '子菜单json',
   PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of menu
+-- Table structure for resources
 -- ----------------------------
-
--- ----------------------------
--- Table structure for `resources`
--- ----------------------------
-DROP TABLE IF EXISTS `resources`;
 CREATE TABLE `resources` (
   `code` varchar(30) NOT NULL COMMENT '编码',
   `title` varchar(30) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '标题',
@@ -30,14 +34,8 @@ CREATE TABLE `resources` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='权限表';
 
 -- ----------------------------
--- Records of resources
+-- Table structure for role
 -- ----------------------------
-INSERT INTO `resources` VALUES ('admin', 'fa', '1', '0', '/user/*', 'fa');
-
--- ----------------------------
--- Table structure for `role`
--- ----------------------------
-DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` varchar(50) NOT NULL,
   `name` varchar(30) NOT NULL COMMENT '角色',
@@ -48,14 +46,8 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
 
 -- ----------------------------
--- Records of role
+-- Table structure for role_menu
 -- ----------------------------
-INSERT INTO `role` VALUES ('1', 'admin', '0', 'fa');
-
--- ----------------------------
--- Table structure for `role_menu`
--- ----------------------------
-DROP TABLE IF EXISTS `role_menu`;
 CREATE TABLE `role_menu` (
   `role_id` varchar(50) DEFAULT NULL,
   `menu_code` varchar(30) DEFAULT NULL,
@@ -64,13 +56,8 @@ CREATE TABLE `role_menu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of role_menu
+-- Table structure for role_resource
 -- ----------------------------
-
--- ----------------------------
--- Table structure for `role_resource`
--- ----------------------------
-DROP TABLE IF EXISTS `role_resource`;
 CREATE TABLE `role_resource` (
   `role_id` varchar(50) DEFAULT NULL,
   `resource_code` varchar(30) DEFAULT NULL,
@@ -79,14 +66,8 @@ CREATE TABLE `role_resource` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of role_resource
+-- Table structure for sys_log
 -- ----------------------------
-INSERT INTO `role_resource` VALUES ('1', 'admin');
-
--- ----------------------------
--- Table structure for `sys_log`
--- ----------------------------
-DROP TABLE IF EXISTS `sys_log`;
 CREATE TABLE `sys_log` (
   `uid` varchar(50) DEFAULT NULL COMMENT '用户ID',
   `content` varchar(600) NOT NULL DEFAULT '' COMMENT '日志内容',
@@ -95,35 +76,24 @@ CREATE TABLE `sys_log` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='操作日志表';
 
 -- ----------------------------
--- Records of sys_log
+-- Table structure for user
 -- ----------------------------
-
--- ----------------------------
--- Table structure for `user`
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` varchar(50) NOT NULL COMMENT '主键ID',
   `username` varchar(20) NOT NULL COMMENT '登录名称',
   `password` varchar(32) NOT NULL COMMENT '密码',
   `email` varchar(60) DEFAULT NULL COMMENT '邮箱',
-  `salt` varchar(50) NOT NULL DEFAULT '0' COMMENT '0、普通用户 1、管理员',
+  `salt` varchar(50) DEFAULT '0' COMMENT '密码的盐',
   `disabled` smallint(6) NOT NULL DEFAULT '1' COMMENT '0、禁用 1、正常',
-  `createTime` datetime NOT NULL COMMENT '创建时间',
-  `lastTime` datetime NOT NULL COMMENT '最后登录时间',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+  `lastTime` datetime DEFAULT NULL COMMENT '最后登录时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `loginname` (`username`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- ----------------------------
--- Records of user
+-- Table structure for user_role
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'root', '5442b02dabc5ed9401be4dfe1ca8adb9', null, 'r', '0', '2016-09-27 19:53:20', '2016-09-27 19:53:22');
-
--- ----------------------------
--- Table structure for `user_role`
--- ----------------------------
-DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
   `uid` varchar(50) NOT NULL,
   `role_id` varchar(50) NOT NULL,
@@ -132,6 +102,13 @@ CREATE TABLE `user_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of user_role
+-- Records 
 -- ----------------------------
+INSERT INTO `menu` VALUES ('system', '系统管理', null, '0', '[{\"code\":\"user\",\"label\":\"用户管理\",\"url\":\"/user\"},{\"code\":\"role\",\"label\":\"角色管理\",\"url\":\"/role\"},{\"code\":\"resource\",\"label\":\"资源管理\",\"url\":\"/resource\"},{\"code\":\"menu\",\"label\":\"菜单管理\",\"url\":\"/menu\"}]');
+INSERT INTO `resources` VALUES ('admin', 'fa', '1', '0', '/user/*', 'fa');
+INSERT INTO `role` VALUES ('1', 'admin', '0', 'fa');
+INSERT INTO `role_menu` VALUES ('1', 'system');
+INSERT INTO `role_resource` VALUES ('1', 'admin');
+INSERT INTO `user` VALUES ('1', 'root', '5442b02dabc5ed9401be4dfe1ca8adb9', null, 'r', '0', '2016-09-27 19:53:20', '2016-09-27 19:53:22');
+INSERT INTO `user` VALUES ('2f66b4f2-3fa7-40fd-b7b1-a7bf28aec6fc', 'rootga', '114056f7bff58af12b6e53e64598503f', null, '0', '0', '2016-10-05 16:19:44', null);
 INSERT INTO `user_role` VALUES ('1', '1');
