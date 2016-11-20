8785ca02146e0764ffbcbca6e83bdd45cafc4095
不迷失 <1060420897@qq.com>
2016-11-20 11:31:56 +0800

    update

--- src/main/resources/public/ngapp/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Created by xieqiang on 2016/9/25.
- */
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_ts_1 = require('./app.module.ts');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_ts_1.AppModule);
-//# sourceMappingURL=main.js.map
--- src/main/resources/public/ngapp/main.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by xieqiang on 2016/9/25.
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module.ts';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
-
